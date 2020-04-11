833d618f245bd2126be5a687e5c19b318be6073a
armaandave <61991339+armaandave@users.noreply.github.com>
2020-04-11 15:56:46 -0700

    commit

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/movie_list')
 def movie_list():
 
-    complete_movie_list = ["Joker", "Frozen 2", "Avengers: Endgame"]
+    complete_movie_list = ["Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame","Joker", "Frozen 2", "Avengers: Endgame"]
 
     return render_template('movie_list.html', complete_movie_list = complete_movie_list)
 
@@ -26,11 +26,14 @@ def movie_page():
     'movie_runtime' : '2h 2min',
     'movie_title' : 'Joker',
     'movie_year' : '(2019)',
+    'movie_opening_weekend': '$96,202,337',
     'movie_domestic_gross' : '$332,224,446',
+    'movie_international_gross': '$738,792,258',
     'movie_worldwide_gross' : '$1,055,824,446',
     'movie_budget' : '$62,500,000',
     'movie_synopsis' : 'During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.'
     
+
     }
 
 
